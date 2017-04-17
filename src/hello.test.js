d7b1526831c81d6877105857b3487ba845338d0b
Carlos Vences <cvences@teamexos.com>
2017-04-16 20:58:49 -0500

    Added local and production servers

--- src/hello.test.js
+++
@@ -1,6 +0,0 @@
-import Greeting from './hello'
-
-test('Greeting.greet', () => {
-  const testGreet = new Greeting('WebApp!')
-  expect(testGreet.greet()).toBe('Semantic Weapons WebApp!')
-})
---
+++ src/shared/config.js
@@ -0,0 +1,5 @@
+// @flow
+
+export const WEB_PORT = process.env.PORT || 3030
+export const STATIC_PATH = '/static'
+export const APP_NAME = 'Semantic Weapons WebApp!'
